2355f397c1a5e1b8ef1e23efc090f49151dd1141
张哲明 <zhangzheming@cocheer.net>
2019-12-12 19:55:10 +0800

    修改页面，修改静态文件引入

--- config/config.default.ts
+++ config/config.default.ts
@@ -1,5 +1,5 @@
 import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
-
+import * as path from 'path';
 export default (appInfo: EggAppInfo) => {
   const config = {} as PowerPartial<EggAppConfig>;
 
@@ -17,8 +17,14 @@ export default (appInfo: EggAppInfo) => {
     url: 'mongodb://129.204.95.132/blog',
     options: {},
   };
-  config.assets = {
-    publicPath: '/public/',
+  config.static = {
+    // 静态化访问前缀,如：`http://127.0.0.1:7001/static/images/logo.png`
+    prefix: '/',
+    dir: path.join(appInfo.baseDir, 'app/public'), // `String` or `Array:[dir1, dir2, ...]` 静态化目录,可以设置多个静态化目录
+    dynamic: true, // 如果当前访问的静态资源没有缓存，则缓存静态文件，和`preload`配合使用；
+    preload: false,
+    maxAge: 31536000, // in prod env, 0 in other envs
+    buffer: true, // in prod env, false in other envs
   };
   config.security = {
     csrf: false,
